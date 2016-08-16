670146557838ed4edc262661925c7acfc6a5a113
Neil F <neilfoxman@gmail.com>
2016-08-15 21:51:45 -0700

    Got printing strings from Putty

--- server.py
+++ server.py
@@ -3,36 +3,38 @@
 import socket
 import sys
 
-HOST = ''
-PORT = 8888
+HOST = '' # symbolic name meaning all available interfaces
+PORT = 8888 # Set port no
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create TCP Socket
 print("Socket created")
 
 try:
-    s.bind((HOST, PORT))
+    s.bind((HOST, PORT)) # Bind to port
 except socket.error as msg:
     print("Bind failed. Error code: "+str(msg[0])+". Message: "+msg[1])
     sys.exit()
 print("Socket bind complete")
 
-s.listen(10)
+s.listen(10) # Listen for a client (backlog up to 10 connections)
 print("Socket now listening")
 
-conn, addr = s.accept()
-print("Connected with "+addr[0]+":"+str(addr[1]))
-conn.sendall(b"yo\n")
+conn, addr = s.accept() # Accept connection, returns new socket connection and address of other end
+print("Connected with IP:"+addr[0]+" at client port:"+str(addr[1]))
+conn.send(b"yo\n\r")
 print("Yo sent")
 
-x = 0
-while x!=1:
-    data = str(conn.recv(1024))
-    if data == b'quit':
-        x = 1
-        print("Should be done. X = "+x)
+data = "doof"
+while data != "q":
+    data = conn.recv(1024).decode("utf-8")
+    print("Before data printed")
     print(data)
-##conn.sendall(data)
+    print("After data printed")
+    if not data:
+        print("Connection closed by client")
+        break
 
-##conn.close()
-##s.close()
-##print("Connection closed")
+conn.close()
+s.close()
+
+
